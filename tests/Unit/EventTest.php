d0634ac121e51bfce593f049ac85dd04ff8d5970
Code-Architect <codearchitectdeveloper@gmail.com>
2017-11-06 12:04:41 +0530

    created test directory and sample test case

---
+++ tests/Unit/EventTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+class EventTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+
+
+
+
+
+}
